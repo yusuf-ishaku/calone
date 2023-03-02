8352c30d54d982507dc302860426565a90935117
yusuf <kingshakzy@gmail.com>
2023-03-02 11:01:02 -0800

    I call Alpha

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,29 @@
 export const Navbar = () =>{
     return(
         <>
-            <div className="w-auto fly mx-16 py-8  h-fit">
-                <span className="w-fit text-md text-here">Caleon</span>
+            <div className="w-auto flex fly mx-16 py-8  h-fit">
+                <span className="block w-fit text-xl text-zen">Caleon</span>
+                <div className="w-fit flex ml-auto items-center text-lg text-relieve text-satoshi">
+                    <span className="block mx-3">Invest</span>
+                    <span className="block mx-3">
+                        Save
+                    </span>
+                    <span className="block mx-3">Blog</span>
+                    <span className="block mx-3">Features</span>
+                    <span className="block mx-3">
+                        Contact Us
+                    </span>
+                    <div className="w-fit flex items-center ml-6">
+                        <span className="block mx-3">
+                            Log In
+                        </span>
+                        <span className="block mx-3">
+                            <button className="w-fit text-blue-100 border-1 rounded-full p-2 px-4">
+                                Start Investing
+                            </button>
+                        </span>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,10 +3,17 @@ export const Home = () =>{
     return(
         <>
             <div className="hero">
-                <div className="outer h-[100vh]">
+                <div className="outer h-fit py-10">
                     <Navbar></Navbar>
-                    <div className="w-full">
-                        This is the home page
+                    <div className="w-full flex px-16 p-4">
+                        <div className="">
+                            <span className="text-3xl text-satoshi text-relieve text-[64px] leading-20 font-black">
+                                Revolutionize the way you save and grow your wealth
+                            </span> 
+                        </div>
+                        <div>
+                            <img src={} alt="" />
+                        </div>
                     </div>
                 </div> 
             </div>
